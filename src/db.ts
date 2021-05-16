12afe909010663e24fb2d39b5b12a6f4ec7ea855
demoadmin <demoadmin@demoadmins-iMac.local>
2021-05-16 13:54:23 -0700

    Add mock teacher data

--- src/db.ts
+++ src/db.ts
@@ -6,22 +6,33 @@ export let students = [
     },
     {
         id: "1c2504f2-b656-11eb-8529-0242ac130003",
-        name: "Anthony Cole",
+        name: "Micheal Bryant",
         teacher: "9c9324e8-b656-11eb-8529-0242ac130003"
     },
     {
         id: "1c2505d8-b656-11eb-8529-0242ac130003",
-        name: "Anthony Cole",
+        name: "Kobe Jordan",
         teacher: "9c9324e8-b656-11eb-8529-0242ac130003"
     },
     {
         id: "1c250696-b656-11eb-8529-0242ac130003",
-        name: "Anthony Cole",
+        name: "Laith Harb",
         teacher: "9c932736-b656-11eb-8529-0242ac130003"
     },
     {
         id: "1c250754-b656-11eb-8529-0242ac130003",
-        name: "Anthony Cole",
+        name: "Kendrick Jenner",
         teacher: "9c932736-b656-11eb-8529-0242ac130003"
+    }
+]
+
+export let teachers = [
+    {
+        id: "9c9324e8-b656-11eb-8529-0242ac130003",
+        name: "Ms Jackson"
+    },
+    {
+        id: "1c250754-b656-11eb-8529-0242ac130003",
+        name: "Mr Wade"
     }
 ]
