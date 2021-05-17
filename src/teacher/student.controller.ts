16a49c255e11ae10a0ca4756395f763d175c2430
demoadmin <demoadmin@demoadmins-iMac.local>
2021-05-16 19:06:55 -0700

    Add teacher dto

--- src/teacher/student.controller.ts
+++ src/teacher/student.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get, Put, Param } from '@nestjs/common';
+import { FindStudentsResponseDto, StudentResponseDto } from '../student/dto/student.dto';
 
 @Controller('teachers/:teacherId/students')
 export class StudentTeacherController {
     @Get()
     getStudents(
         @Param('teacherId') teacherId: string 
-    ) {
+    ): FindStudentsResponseDto[] {
        return `Get All Students That Belong To A Teacher With Id of ${teacherId}`
     }
 
@@ -13,7 +14,7 @@ export class StudentTeacherController {
     updateStudentTeacher(
         @Param('teacherId') teacherId: string,
         @Param('studentId') studentId: string
-    ) {
+    ): StudentResponseDto {
         return `Update Student With Id Of ${studentId} To Teacher With Id Of ${teacherId}`
     }
 }
--- src/teacher/teacher.controller.ts
+++ src/teacher/teacher.controller.ts
@@ -1,16 +1,17 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { FindTeacherResponseDto } from './dto/teacher.dto';
 
 @Controller('teachers')
 export class TeacherController {
     @Get()
-    getTeachers() {
+    getTeachers(): FindTeacherResponseDto[] {
         return "All Teachers"
     }
 
     @Get('/:teacherId')
     getTeacherById(
         @Param('teacherId') teacherId: string
-    ){
+    ): FindTeacherResponseDto {
         return `Get Teacher With Id Of ${teacherId}`
     }
 }
